d1435563295a77a328a6d342ac4b63bef712689b
Liguo Jiao <liguojiao@liguos-imac.mckay.co.nz>
2015-11-20 17:06:04 +1300

    Basic function done
    - Change to iOS9 version add fbauth2 to LSApplicationQueriesSchemes
    - demo function done

--- Facebook_loggin/Facebook_loggin/ViewController.swift
+++ Facebook_loggin/Facebook_loggin/ViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        if error == nil {
+            self.performSegueWithIdentifier("show", sender: self)
+            print("Hacked")
+            
+        } else {
+            print(error.localizedDescription)
+        }
+    }
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        print("logout")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let loginButton = FBSDKLoginButton()
+        loginButton.readPermissions = ["public_profile","email","user_friends"]
+        loginButton.center = self.view.center
+        loginButton.delegate = self
+        self.view.addSubview(loginButton)
         // Do any additional setup after loading the view, typically from a nib.
+        if FBSDKAccessToken.currentAccessToken() == nil {
+            print("No one logged in")
+        } else {
+            print("Here you are, you logged in now")
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
